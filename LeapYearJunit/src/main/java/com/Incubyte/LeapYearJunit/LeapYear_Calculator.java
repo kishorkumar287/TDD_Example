96b5983697cb8733c81c2390d6d1aa4a1c1b3622
kishorkumar287 <60978027+kishorkumar287@users.noreply.github.com>
2020-05-14 07:41:52 +0530

    FirstCriteria tested.

--- LeapYearJunit/src/main/java/com/Incubyte/LeapYearJunit/LeapYear_Calculator.java
+++ LeapYearJunit/src/main/java/com/Incubyte/LeapYearJunit/LeapYear_Calculator.java
@@ -1,5 +1,14 @@
 package com.Incubyte.LeapYearJunit;
 
 public class LeapYear_Calculator {
+	
+	
+	public boolean firstCriteria(int date)
+	{
+		if(date % 400 == 0)
+			return true;
+		
+		return false;
+	}
 
 }
--- LeapYearJunit/src/test/java/com/Incubyte/LeapYearJunit/Test_LeapYear_Calculator.java
+++ LeapYearJunit/src/test/java/com/Incubyte/LeapYearJunit/Test_LeapYear_Calculator.java
@@ -1,7 +1,19 @@
 package com.Incubyte.LeapYearJunit;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
 public class Test_LeapYear_Calculator  {
+	
+	LeapYear_Calculator cal = new LeapYear_Calculator();
+	
+	@Test
+	public void testFirstCriteria()
+	{
+		assertEquals("first criteria : Divisible by 400", true, cal.firstCriteria(400) );
+	}
 
 }
